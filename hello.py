5928011d44a7c16c7c22a1b345a16f5593e8160c
hardly <trigal@qq.com>
2018-03-16 20:59:11 +0800

    use mysql

---
+++ hello.py
@@ -0,0 +1,93 @@
+
+# -*- coding: utf-8 -*-
+from flask import Flask,render_template,session,redirect,url_for,flash
+from flask_bootstrap import Bootstrap
+from datetime import datetime
+from flask_moment import Moment
+from flask_script import Manager
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+import pymysql
+from flask_migrate import Migrate, MigrateCommand
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'haha'
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:123321@localhost:3306/flasky?charset=utf8' 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True #设置这一项是每次请求结束后都会自动提交数据库中的变动
+
+
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+db = SQLAlchemy(app) #实例化
+manager = Manager(app)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+# 创建迁移仓库python hello.py db init
+# 创建迁移脚本python hello.py db migrate -m "initial migration"
+# 把迁移应用到数据库python hello.py db upgrade
+
+'''定义模型，建立关系'''
+class Role(db.Model):
+    # 定义表名
+    __tablename__ = 'roles'
+    # 定义列对象
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+    #repr()方法显示一个可读字符串，虽然不是完全必要，不过用于调试和测试还是很不错的。
+    def __repr__(self):
+        return '<Role {}> '.format(self.name)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?',validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    render_template('404.html'),404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    render_template('500.html'),500
+
+@app.route('/',methods = ['GET','POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'),
+                           known=session.get('known', False))
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html',name=name)
+
+
+if __name__ == '__main__':
+	manager.run()
+	#app.run(debug=True)
