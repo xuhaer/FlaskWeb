f2be735c1ee079163db1115b44560263eb677cf1
hardly <trigal@qq.com>
2018-03-18 17:26:01 +0800

    使用flask-mail 发送邮件

--- hello.py
+++ hello.py
@@ -1,36 +1,37 @@
 
 # -*- coding: utf-8 -*-
-from flask import Flask,render_template,session,redirect,url_for,flash
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from datetime import datetime
 from flask_moment import Moment
 from flask_script import Manager
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 import pymysql
 from flask_migrate import Migrate, MigrateCommand
 import os
 from flask_mail import Mail, Message
+from threading import Thread
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'haha'
-app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:123321@localhost:3306/flasky?charset=utf8' 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True #设置这一项是每次请求结束后都会自动提交数据库中的变动
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123321@localhost:3306/flasky?charset=utf8'
+# 设置这一项是每次请求结束后都会自动提交数据库中的变动
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['MAIL_SERVER'] = 'smtp.163.com'
 app.config['MAIL_PORT'] = 25
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
-app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
-app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <flasky@example.com>'
+app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin<trigal@163.com>'
 app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
 
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
-db = SQLAlchemy(app) #实例化
+db = SQLAlchemy(app)  # 实例化
 manager = Manager(app)
 migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
@@ -41,6 +42,8 @@ mail = Mail(app)
 
 
 '''定义模型，建立关系'''
+
+
 class Role(db.Model):
     # 定义表名
     __tablename__ = 'roles'
@@ -48,9 +51,11 @@ class Role(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
     users = db.relationship('User', backref='role', lazy='dynamic')
-    #repr()方法显示一个可读字符串，虽然不是完全必要，不过用于调试和测试还是很不错的。
+    # repr()方法显示一个可读字符串，虽然不是完全必要，不过用于调试和测试还是很不错的。
+
     def __repr__(self):
         return '<Role {}> '.format(self.name)
+
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -61,9 +66,26 @@ class User(db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
+def send_email(to, subject, template, **kwargs):
+    msg = Message(subject,
+                  sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
+
+
 class NameForm(FlaskForm):
-    name = StringField('What is your name?',validators=[DataRequired()])
+    name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
 
 @app.shell_context_processor
 def make_shell_context():
@@ -72,13 +94,15 @@ def make_shell_context():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    render_template('404.html'),404
+    render_template('404.html'), 404
+
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    render_template('500.html'),500
+    render_template('500.html'), 500
 
-@app.route('/',methods = ['GET','POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
@@ -88,6 +112,9 @@ def index():
             db.session.add(user)
             db.session.commit()
             session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_email(app.config['FLASKY_ADMIN'], 'New User',
+                           'mail/new_user', user=user)
         else:
             session['known'] = True
         session['name'] = form.name.data
@@ -95,11 +122,12 @@ def index():
     return render_template('index.html', form=form, name=session.get('name'),
                            known=session.get('known', False))
 
+
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html',name=name)
+    return render_template('user.html', name=name)
 
 
 if __name__ == '__main__':
-	manager.run()
-	#app.run(debug=True)
+    manager.run()
+    # app.run(debug=True)
