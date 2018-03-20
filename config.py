bdd7c94735a81419d9271ff1e803403e0a16519c
hardly <trigal@qq.com>
2018-03-20 21:54:46 +0800

    分页显示

--- config.py
+++ config.py
@@ -11,6 +11,7 @@ class Config:
     FLASKY_MAIL_SENDER= 'Flasky Admin<trigal@163.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    FLASKY_POSTS_PER_PAGE = 2
 
     @staticmethod
     def init_app(app):
