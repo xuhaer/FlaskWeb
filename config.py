a6e5c292ae8c8cf7d7aae7a6e9e1b644e6db67c4
hardly <trigal@qq.com>
2018-03-23 12:14:35 +0800

    comments_moderater

--- config.py
+++ config.py
@@ -14,6 +14,7 @@ class Config:
     #SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     FLASKY_POSTS_PER_PAGE = 5
     FLASKY_FOLLOWERS_PER_PAGE = 10
+    FLASKY_COMMENTS_PER_PAGE = 5
 
     @staticmethod
     def init_app(app):
