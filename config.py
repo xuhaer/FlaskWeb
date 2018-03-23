6d74a0c27880db7decb8b626153dcc336397b684
hardly <trigal@qq.com>
2018-03-23 10:44:56 +0800

    followers

--- config.py
+++ config.py
@@ -8,10 +8,12 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    FLASKY_MAIL_SENDER= 'Flasky Admin<trigal@163.com>'
+    FLASKY_MAIL_SENDER= 'Flask  y Admin<trigal@163.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    FLASKY_POSTS_PER_PAGE = 2
+    #SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    FLASKY_POSTS_PER_PAGE = 5
+    FLASKY_FOLLOWERS_PER_PAGE = 10
 
     @staticmethod
     def init_app(app):
